ab63484fb848704e4f91bfb77c58ff1859f73a7e
wanxiaodong <729779978@qq.com>
2019-03-18 19:00:26 +0800

    dockerfile

--- jenkins/buile.sh
+++ jenkins/buile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build --no-cache --rm -f ./dockerfile -t node-server ./
+docker push wanxiaodong/node-server:0.0.1
--- jenkins/start.sh
+++ jenkins/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo 'started'
+docker pull wanxiaodong/node-server
+docker run -itd --rm --name=node-server -p 3120:3120 wanxiaodong/node-server
